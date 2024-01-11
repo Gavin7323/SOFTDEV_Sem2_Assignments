f5d195114e2e3b5d9024e16baf8d4f8c02467975
Gavin Wilson <wilsogav000@warren.k12.in.us>
2024-01-11 10:39:32 -0500

    updates

--- 04_Try Catch Finally/Assignment/src/App.java
+++ 04_Try Catch Finally/Assignment/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("EnterNumber: ");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter another number: ");
+        int secondNumber = 1;
+
+        int quotient = 0;
+        try{
+            quotient = firstNumber / secondNumber;
+        } catch (ArithmeticException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Defalut answer of 0 used for quotient");
+
+        }
     }
 }
--- 04_Try Catch Finally/Assignment/src/FibNumberException.java
+++
@@ -1,5 +0,0 @@
-public class FibNumberException extends Exception{
-    public FibNumberException(String message){
-        super(message);
-    }
-}
