04695e662bac817ac8afe083dfe63b275c73297d
Gavin Wilson <wilsogav000@warren.k12.in.us>
2024-02-02 10:17:04 -0500

    sus

--- 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
+++ 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
@@ -1,5 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double x = 7;
+        double y = x / 5;
+        double z = x + y;
+        System.out.println(z);
+
     }
 }
