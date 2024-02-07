6736da7e376f9989a8eece277edfcc1a6e76aebb
Gavin Wilson <wilsogav000@warren.k12.in.us>
2024-02-07 10:16:14 -0500

    g

--- 04_Functional Programming_Streams/01_Review of Functional Programming/Assignment/src/App.java
+++ 04_Functional Programming_Streams/01_Review of Functional Programming/Assignment/src/App.java
@@ -1,5 +1,48 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        String name = convertNumberToCarName.apply(0);
+        String longName = convertNameToLongName.apply(name);
+
+        // (int -> String)
+        var convertNumberToLongName = convertNumberToCarName.andThen(convertNameToLongName);
+        convertNumberToLongName.apply(0);
+
+        var convertNumbertoLongName2 = convertNameToLongName.compose(convertNumberToCarName);
+
     }
+
+    // (int) -> (String)
+    public static Function<Integer, String> convertNumberToCarName = (number) -> {
+        if (number == 0) {
+            return "Chevy";
+        } else {
+            return "Some Other Car";
+        }
+    };
+
+    // (string) -> (String)
+    public static Function<String, String> convertNameToLongName = (name) -> {
+        if (name == "Chevy") {
+            return "Chevrolet";
+        } else {
+            return "Some other Unknown Car Type Name";
+        }
+    };
+
+    // // (int, String) -> (String)
+    // // (int) -> (String) -> (String)
+    // public static Function<Integer, Function<String, String>> convertNumberToLongName = (number) -> (shortName) -> {
+    //     convertNumberToCarName.apply(null)
+        
+    // };
+
+    public static Consumer<Integer> consumeInt = (num) -> System.out.println(num);
+    public static Supplier<Double> supplyThree = () -> 3.0;
+    public static Runnable doSomething = () -> System.out.println("Sleep Painfully");
+
 }
