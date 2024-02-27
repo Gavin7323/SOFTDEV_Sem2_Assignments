6f5c9b9e19ea3712b852577f30a35c07b54747c5
Gavin Wilson <wilsogav000@warren.k12.in.us>
2024-02-27 10:14:44 -0500

    bobissus

--- Unit_06_Parallel Programming/Intro/Assignment/src/App.java
+++ Unit_06_Parallel Programming/Intro/Assignment/src/App.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        for (int i = 0; i < 10; i++) {
+        RunnableDemo demo = new RunnableDemo("Thread-" + i);
+        demo.start();
+        }
+
+
+
     }
 }
